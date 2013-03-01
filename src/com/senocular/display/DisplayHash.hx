85ad6328af276c5d78d5066c7e3f376f400e0c02
Jaroslav Meloun <jaroslav.meloun@gmail.com>
2013-03-01 15:04:50 +0100

    maybe done

---
+++ src/com/senocular/display/DisplayHash.hx
@@ -0,0 +1,60 @@
+package com.senocular.display;
+import nme.display.DisplayObject;
+
+/**
+ * ...
+ * @author Jarnik
+ */
+class DisplayHash<T>
+{
+	var keys:Array<DisplayObject>;
+	var values:Array<T>;
+
+	public function new() 
+	{
+		keys = [];
+		values = [];
+	}
+	
+	public function contains( d:DisplayObject ):Bool {
+		for ( k in keys )
+			if ( k == d )
+				return true;
+		return false;
+	}
+	
+	public function set( d:DisplayObject, v:T ):Void {
+		var index:Int = -1;
+		for ( i in 0...keys.length ) {
+			if (keys[ i ] == d ) {
+				index = i;
+				break;
+			}
+		}
+		if ( index == -1 ) {
+			keys.push( d );
+			values.push( v );
+		} else {
+			values[ index ] = v;
+		}
+	}
+	
+	public function remove( d:DisplayObject ):Void {
+		for ( i in 0...keys.length ) {
+			if (keys[ i ] == d ) {
+				keys.splice( i, 1 );
+				values.splice( i, 1 );
+				return;
+			}
+		}
+	}
+	
+	public function get( d:DisplayObject ):T {
+		for ( i in 0...keys.length ) {
+			if (keys[ i ] == d ) {
+				return values[ i ];
+			}
+		}
+		return null;
+	}
+}
